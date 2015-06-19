6af7d6132c7cb1966bf905bfe2946d547dec7c3d
rebeccacaroline <rebeccaholzschuh@gmail.com>
2015-06-19 10:50:25 -0700

    starting on front end- about to start templating with handlebars

--- app/assets/javascripts/meters.js
+++ app/assets/javascripts/meters.js
@@ -7,19 +7,30 @@ $(document).ready(function(){
       url: "/",
       data: {city: city_name}
     }).success(function(response){
-      $('.container').append(buildMeterTemplate(response["meter"]))
+      iterateMeters(response['meters']);
       $('.container').append(buildSavingsTemplate(response["savings"]))
 
     }).fail(function(){
-      console.log("You Fail!")
+      $('.container').append("<h2>That city could not be found</h2>")
     })
   });
 
   function buildMeterTemplate(meterInfo){
-
+    $('.meters ul li').text(meterInfo);
   };
 
   function buildSavingsTemplate(savingsInfo){
+    $('.savings .water').text(savingsInfo['water']);
+    $('.savings .gas').text(savingsInfo['gas']);
+    $('.savings .electric').text(savingsInfo['electric']);
+    $('.savings .total').text(savingsInfo['total']);
+  };
 
-  }
+  function iterateMeters(meters){
+    console.log(meters);
+    for (i = 0; i < meters.length; i++ ) {
+      console.log(meters[i])
+      $('.container').append(buildMeterTemplate(meters[i]));
+    };
+  };
 });
--- app/helpers/meters_helper.rb
+++ app/helpers/meters_helper.rb
@@ -1,2 +1,22 @@
 module MetersHelper
+  def calculate_savings(meters)
+    electricity_savings = []
+    water_savings = []
+    gas_savings = []
+    meters.each do |meter|
+      case meter.utility
+      when "electric"
+        electricity_savings << meter.dollar_amount_saved
+      when "gas"
+        gas_savings << meter.dollar_amount_saved
+      when "water"
+        water_savings << meter.dollar_amount_saved
+      end
+    end
+    electricity_savings = electricity_savings.reduce(:+).round(2)
+    gas_savings = gas_savings.reduce(:+).round(2)
+    water_savings = water_savings.reduce(:+).round(2)
+    total_savings = electricity_savings + gas_savings + water_savings.round(2)
+    return [electricity_savings, gas_savings, water_savings, total_savings]
+  end
 end
