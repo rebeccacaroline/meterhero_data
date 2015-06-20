faecd0de14a20b03337b3caaef01c0d3f9ecb34c
rebeccacaroline <rebeccaholzschuh@gmail.com>
2015-06-20 11:19:46 -0700

    tests for model

--- spec/models/meter_spec.rb
+++ spec/models/meter_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Meter, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it {should have_db_column(:utility)}
+
+  it {should have_db_column(:units_saved)}
+
+  it {should have_db_column(:dollar_amount_saved)}
+
+  it {should have_db_column(:city)}
+
+  it {should have_db_column(:state)}
+
 end
